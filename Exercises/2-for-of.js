baee7ea4aded766762ba98e46efdcc2149bdadcd
ariia horunzha <1mpellee@gmail.com>
2024-11-05 23:40:47 +0200

    first commit

--- Exercises/2-for-of.js
+++ Exercises/2-for-of.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const sum = (...args) => {
-  // Use for..of loop and accumulator variable
-  // to calculate sum of all given arguments
-  // For example sum(1, 2, 3) should return 6
+  let res = 0;
+  for (const i of args) {
+    res += i;
+  }
+  return res;
 };
 
 module.exports = { sum };
